9c8b5a2e5a2af1672d0c84f6efa87fe47d015bd8
KouameAk <akazakouame1@gmail.com>
2024-11-08 15:01:51 +0100

    Update TP2exo8.py

--- TP2/TP2exo8.py
+++ TP2/TP2exo8.py
@@ -1,5 +1,5 @@
 def inter():
-    x = int(input("Entrez un nombre décimal :"))
+    x = float(input("Entrez un nombre décimal :"))
 
     if (x>=2 and x<3) or (x>0 and x<=1) or (x>=-10 and x<-2):
         print("x appartient à I")
